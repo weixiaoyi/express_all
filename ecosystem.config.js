85bba3a25036c4e4b17690310e36b3f2a43b8713
weixiaoyi <2278095567@qq.com>
2018-11-10 12:41:59 +0800

    起始文件

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: './bin/www',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
