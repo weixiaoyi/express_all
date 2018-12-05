0e82315e470df649e35454e84e6f7639b7c6afee
weixiaoyi <2278095567@qq.com>
2018-12-05 18:51:42 +0800

    修复问题

--- routes/users.js
+++ routes/users.js
@@ -1,67 +1,75 @@
 const fs = require('fs');
 const express = require('express');
-const Client = require('ssh2').Client;
 const archiver = require('archiver');
+const Client = require('ssh2').Client;
 
 const router = express.Router();
-const output = fs.createWriteStream('D:/myproject/rxjs_mobx' + '/build.zip');
-const archive = archiver('zip', {
-  zlib: { level: 9 } // Sets the compression level.
-});
 
-class Ssh{
-  constructor(){
 
-  }
 
-}
+// class Ssh{
+//   constructor(){
+//
+//   }
+//
+// }
 
 router.get('/', function(req, res) {
-  const conn = new Client();
-  conn.on('ready', function() {
-    console.log('Client :: ready')
-    conn.sftp(function(err, sftp) {
-      if (err) throw err;
-      output.on('end', function () {
-        res.send('end')
-      })
-      output.on('close', function () {
-        return res.send('压缩成功')
-        // conn.exec('cd /myprojects/rxjs_mobx/build && rm -rf **',(err,stream)=>{
-        //   if(!err){
-        //     sftp.fastPut('D:/myproject/rxjs_mobx/build.zip','/myprojects/rxjs_mobx/build.zip',function(err){
-        //       if(!err){
-        //         conn.exec('cd /myprojects/rxjs_mobx && unzip build.zip -d build',(err,stream)=>{
-        //           if(!err){
-        //             res.send('删除文件成功&&替换文件成功&&解压成功')
-        //           }
-        //           conn.end()
-        //         })
-        //       }else{
-        //         res.send(err)
-        //         console.log(err,'------------')
-        //       }
-        //     })
-        //   }
-        // })
-
-      })
-      archive.on('error', function (err) {
-        res.send('压缩文件失败')
-      })
-      archive.pipe(output);
-      archive.directory('D:/myproject/rxjs_mobx/build/',false)
-      archive.finalize()
-    });
-
-  }).connect({
-    host: '47.244.59.36',
-    port: 22,
-    username: 'root',
-    password: 'Weixiaoyao886'
+  const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
   });
 
+  const output = fs.createWriteStream('D:/myproject/rxjs_mobx' + '/build.zip');
 
+  output.on('end', function () {
+    res.send('end')
+  })
+  output.on('close', function () {
+
+
+    const conn = new Client();
+
+    conn.on('ready', function() {
+      console.log('Client :: ready')
+      conn.sftp(function(err, sftp) {
+        if (err) throw err;
+        conn.exec('cd /myprojects/rxjs_mobx/build && rm -rf **',(err,stream)=>{
+          if(!err){
+            sftp.fastPut('D:/myproject/rxjs_mobx/build.zip','/myprojects/rxjs_mobx/build/build.zip',function(err){
+              if(!err){
+                conn.exec('cd /myprojects/rxjs_mobx/build/ && unzip build.zip',(err,stream)=>{
+                  if(!err){
+                    res.send('删除文件成功&&替换文件成功&&解压成功')
+                  }
+                  conn.end()
+                })
+              }else{
+                res.send(err)
+                console.log(err,'------------')
+              }
+            })
+          }
+        })
+
+        // res.send('压缩成功')
+        // conn.end()
+      });
+
+    }).connect({
+      host: '47.244.59.36',
+      port: 22,
+      username: 'root',
+      password: 'Weixiaoyao886'
+    });
+  })
+
+  archive.on('error', function (err) {
+    console.log(err,'----------------------------------------')
+  })
+
+  archive.pipe(output)
+  archive.directory('D:/myproject/rxjs_mobx/build/',false)
+  archive.finalize()
 });
 
 module.exports = router;
